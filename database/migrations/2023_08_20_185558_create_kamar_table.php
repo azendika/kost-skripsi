d458100c8a1ae26fcf0e5b21ae7c4addcb01a98b
zulfikars08 <98329041+zulfikars08@users.noreply.github.com>
2023-08-21 15:49:43 +0700

    Add files via upload

---
+++ database/migrations/2023_08_20_185558_create_kamar_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKamarTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kamar', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('kost_id');
+            $table->string('no_kamar');
+            $table->integer('harga');
+            $table->string('fasilitas');
+            $table->string('keterangan');
+            $table->timestamps();
+            $table->integer('created_by')->nullable()->unsigned();
+            $table->integer('updated_by')->nullable()->unsigned();
+            $table->integer('deleted_by')->nullable()->unsigned();
+            $table->dateTime('deleted_at')->nullable()->default(null);
+            
+            $table->foreign('kost_id')->references('id')->on('kamar')
+            ->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kamar');
+    }
+}
